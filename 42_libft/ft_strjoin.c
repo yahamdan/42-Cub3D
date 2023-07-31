bcc129906ab018477e3ddf57b3dd1ee85f455f84
Walid Errahmani <werrahma@e1r5p10.1337.ma>
2023-07-31 14:14:30 +0100

    libft

---
+++ 42_libft/ft_strjoin.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: werrahma <werrahma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/21 11:09:10 by werrahma          #+#    #+#             */
+/*   Updated: 2022/10/28 15:28:38 by werrahma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		a;
+	int		b;
+	char	*m;
+
+	a = -1;
+	b = -1;
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1 && s2)
+		return ((char *)s2);
+	if (s1 && !s2)
+		return ((char *)s1);
+	m = malloc((ft_strlen(s1) + ft_strlen(s2)) + 1);
+	if (!m)
+		return (0);
+	while (s1[++a])
+		m[a] = s1[a];
+	while (s2[++b])
+		m[a + b] = s2[b];
+	m[a + b] = '\0';
+	return (m);
+}
